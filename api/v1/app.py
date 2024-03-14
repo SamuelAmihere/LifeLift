2c1e9117be116933587528ef3f0396f19ad173d6
Samuel <amisam2000@gmail.com>
2024-03-14 23:00:49 +0800

    modified:   api/v1/app.py
    Changes not staged for commit:
    	modified:   api/v1/views/__init__.py
    	modified:   api/v1/views/health_messages.py
    	modified:   api/v1/views/hospitals.py
    	modified:   api/v1/views/index.py
    	modified:   models/__init__.py
    	modified:   models/engine/db_storage.py
    	modified:   models/hosp_operator.py
    	modified:   models/system_user.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,24 +3,35 @@
 from models import storage
 from api.v1.views import app_views
 from os import environ
-from flask import Flask, render_template, make_response, jsonify, session
-from flask_cors import CORS
+from flask import Flask, render_template, make_response, jsonify #session
+from flask_cors import CORS, cross_origin
 from flasgger import Swagger
 from flasgger.utils import swag_from
 
+
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.register_blueprint(app_views)
+swagger = Swagger(app)
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-app.secret_key = 'super secret key'
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-session(app)
+app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
+# global strict slashes
+# app.url_map.strict_slashes = False
 
+# app.secret_key = 'super secret key'
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# session(app)
+
+host = environ.get('LFTLIFT_API_HOST', '0.0.0.0')
+port = environ.get('LFTLIFT_API_PORT', 5000)
+   
 @app.teardown_appcontext
-def close_db(error):
-    """ Close Storage """
+def teardown_db(exception):
+    """
+    after each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
     storage.close()
 
 
@@ -32,23 +43,24 @@ def not_found(error):
       404:
         description: a resource was not found
     """
+    print(error)
     return make_response(jsonify({'error': "Not found"}), 404)
 
 app.config['SWAGGER'] = {
-    'title': 'AirBnB clone Restful API',
-    'uiversion': 3
+    'title': 'LifeLisft clone Restful API',
+    'uiversion': 1
 }
 
-Swagger(app)
+
 
 
 if __name__ == "__main__":
-    app.config['SESSION_TYPE'] = 'filesystem'
+    # app.config['SESSION_TYPE'] = 'filesystem'
     app.debug = True
-    port = environ.get('LIFELIFT_API_PORT')
-    if port is None:
-        port = 5000
-    host = environ.get('LIFELIFT_API_HOST')
-    if host is None:
-        host = 'localhost'
+    # port = environ.get('LIFELIFT_API_PORT')
+    # if port is None:
+    #     port = 5000
+    # host = environ.get('LIFELIFT_API_HOST')
+    # if host is None:
+    #     host = 'localhost'
     app.run(host=host, port=port, threaded=True)
